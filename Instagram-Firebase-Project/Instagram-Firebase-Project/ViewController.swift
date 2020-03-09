56a3eae9b2edeb897ab59b6bd2f6eb43d30d190f
Chelsi Christmas <c.christmas95@gmail.com>
2020-03-09 16:19:53 -0400

    Added xcode project file

---
+++ Instagram-Firebase-Project/Instagram-Firebase-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram-Firebase-Project
+//
+//  Created by Chelsi Christmas on 3/9/20.
+//  Copyright © 2020 Chelsi Christmas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
